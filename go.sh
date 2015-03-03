fe0ea5fe9f2351bc9046762a6e746b964848ddfa
Davide Guerri <davide.guerri@hp.com>
2015-03-03 18:11:53 +0000

    Download boot2docker image if not present

--- go.sh
+++ go.sh
@@ -2,6 +2,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+vagrant box list | grep wearableintelligence/boot2docker-parallels >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  curl -LO https://github.com/wearableintelligence/boot2docker-vagrant-box/releases/download/docker%2Fv1.5.0/boot2docker-parallels.box
+  vagrant box add --name wearableintelligence/boot2docker-parallels boot2docker-parallels.box
+  rm boot2docker-parallels.box
+fi
+
 export VAGRANT_DEFAULT_PROVIDER=parallels
 
 vagrant destroy -f
